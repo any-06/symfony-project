94bb0dcf822871f94f373f578bbc7dcca3d45072
any-06 <anyou.6.ali@gmail.com>
2022-08-19 10:49:06 +0200

    install yarn

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,21 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
         $user = new User();
-        $user->setPrenom('Stef')
-            ->setNom('BK')
-            ->setAge(50)
-            ->setUsername('stefbk')
-            ->setEmail('stef@stef.com')
-            ->setPassword('0000')
+        $user->setPrenom('Anyou')
+            ->setNom('Ali')
+            ->setAge(27)
+            ->setUsername('any-06')
+            ->setEmail('anyou@anyou.com')
+            ->setPassword($this->hasher->hashPassword($user, 'test1234'))
             ->setRoles(["ROLE_ADMIN"])
             ->setVille('VALENCE');
 
