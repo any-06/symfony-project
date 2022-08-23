ff1a9127842e7d5a5e8135c8574b3ac120a87ba1
any-06 <anyou.6.ali@gmail.com>
2022-08-23 10:30:06 +0200

    feat(categorie): create custom query for categorie visibility and refactor views

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,12 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleType extends AbstractType
 {
@@ -16,11 +19,23 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('titre', TextType::class, [
-                'label' => 'Titre:',
+                'label' => 'Titre :',
                 'required' => true
             ])
+            ->add('categories', EntityType::class, [
+                'class' => Categorie::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->andWhere('c.enable = true')
+                        ->orderBy('c.titre', 'ASC');
+                },
+                'label' => 'Catégories :',
+                'choice_label' => 'titre',
+                'multiple' => true,
+                'by_reference' => false
+            ])
             ->add('content', TextareaType::class, [
-                'label' => 'Contenu:',
+                'label' => 'Contenu :',
                 'required' => true
             ]);
         // ->add('save', SubmitType::class, [
