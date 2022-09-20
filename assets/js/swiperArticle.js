5d5a18c1f4a0405e4a37d20007d9338532dc7c79
any-06 <anyou.6.ali@gmail.com>
2022-09-20 15:02:51 +0200

    feat(article): add autoplay and make slide clickable

--- assets/js/swiperArticle.js
+++ assets/js/swiperArticle.js
@@ -1,4 +1,4 @@
-import Swiper from "swiper";
+import Swiper from "swiper/bundle";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -10,10 +10,22 @@ const swiper = new Swiper(".swiper-image", {
     delay: 3000,
     disableOnInteraction: true,
   },
+  speed: 1000,
   grabCursor: true,
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
   },
 });
